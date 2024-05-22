d1f6866f0f91fedc402855271858a71565fe8373
SANaoi <3144347657@qq.com>
2024-05-22 17:37:34 +0800

    添加roomGUI

--- Assets/HotUpdateProgress/Manager/RoomManager_Net.cs
+++ Assets/HotUpdateProgress/Manager/RoomManager_Net.cs
@@ -5,4 +5,30 @@ using Mirror;
 
 public class RoomManager_Net : NetworkRoomManager
 {
+    bool IsShowStartButton;
+
+    public override void OnRoomServerPlayersReady()
+    {
+        if (Utils.IsHeadless())
+        {
+            base.OnRoomServerPlayersReady();
+        }
+        else
+        {
+            IsShowStartButton = true;
+        }
+    }
+
+    public override void OnGUI()
+    {
+        base.OnGUI();
+
+        if (allPlayersReady && IsShowStartButton  && GUI.Button(new Rect(150, 300, 120, 20), "START GAME"))
+        {
+            // set to false to hide it in the game scene
+            IsShowStartButton = false;
+
+            ServerChangeScene(GameplayScene);
+        }
+    }
 }
